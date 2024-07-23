e2415a8ac97fd8455fd8f76f533160f42fe4bbe7
Joseph-Chege <mjchege23@gmail.com>
2024-07-23 09:44:20 +0300

    Created the MySet class data structure and added methods

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,41 @@
 class MySet:
+    def __init__(self, enumerable = []):
+        self.dictionary = {}
+        for value in enumerable:
+            self.dictionary[value] = True
 
-    pass
+        pass
+
+    def has(self, value):
+        return value in self.dictionary
+    
+    def add(self, value):
+        self.dictionary[value] = True
+        return self
+    
+    def delete(self, value):
+        self.dictionary.pop(value, None)
+        return self
+    
+    def size(self):
+        return len(self.dictionary)
+    
+    def clear(self):
+        self.dictionary = {}
+        return self
+    
+    def __str__(self):
+        set_list = []
+
+        for key, value in self.dictionary.items():
+            set_list.append(f"{key}: {value}")
+        
+        return f'MySet: {{ {", ".join(set_list)}}}'
+
+
+    
+        
+
+
+
+ 
